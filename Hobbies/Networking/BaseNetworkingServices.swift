f47ad0c1cc2ff1106e59a69e898ace7d31f7c2f1
aakpro <aakpro@gmail.com>
2019-05-18 09:59:01 +0300

    initialize base networking service

---
+++ Hobbies/Networking/BaseNetworkingServices.swift
@@ -0,0 +1,16 @@
+//
+//  BaseNetworkingServices.swift
+//  Hobbies
+//
+//  Created by Amir Abbas Kashani on 5/18/19.
+//  Copyright © 2019 companyA. All rights reserved.
+//
+
+import Alamofire
+import ObjectMapper
+import Foundation
+
+public class BaseNetworkingServices: NSObject
+{
+
+}
