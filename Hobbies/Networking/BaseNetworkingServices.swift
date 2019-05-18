1c0058a914edb023642f10b3ab5d9ab191c106b9
aakpro <aakpro@gmail.com>
2019-05-18 10:30:17 +0300

    added base networking and api models

--- Hobbies/Networking/BaseNetworkingServices.swift
+++ Hobbies/Networking/BaseNetworkingServices.swift
@@ -12,5 +12,29 @@ import Foundation
 
 public class BaseNetworkingServices: NSObject
 {
-
+    internal var baseURL = "https://raw.githubusercontent.com/AmirDaliri/getTestJson/master/"
+    public func request(endpoint:String,
+                        method:HTTPMethod,
+                        body:Dictionary<String,Any>!,
+                        headers: Dictionary<String,String>! = nil,
+                        encoding: ParameterEncoding = JSONEncoding.default,
+                        completionHandler: @escaping (Result<Any>) -> Void)
+    {
+        let dataRequest = Alamofire.request("\(self.baseURL)\(endpoint)",
+            method: method,
+            parameters: body,
+            encoding: encoding,
+            headers: headers)
+        dataRequest.responseJSON { (dataResponse) in
+            switch dataResponse.result {
+            case .success(let successResult):
+                completionHandler(.success(successResult))
+                break
+            case .failure(let errorResult):
+                completionHandler(.failure(errorResult))
+                break
+            }
+        }
+        
+    }
 }
